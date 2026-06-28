ac2682557073de5348e92991b4286db72c3d0c1f
Osamudiamen Amayo <osamudiamenamayo@MacBookAir.powerhub>
2026-06-28 12:38:19 +0100

    Homepage redesign with instant quote

---
+++ src/app/booking/booking/page.tsx
@@ -0,0 +1,7 @@
+//export {default} from "@/components/HomePage";
+//import HomePage from "@/components/HomePage";
+"use client";
+import BookingPage from "@/components/BookingPage";
+export default function Page() {
+  return <BookingPage />;
+}
